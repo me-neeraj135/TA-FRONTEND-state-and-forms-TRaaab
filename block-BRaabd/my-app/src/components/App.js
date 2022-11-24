3bcddb269206421133c5beb3cc63dd437fcbd2cf
neeraj <me.neeraj1455@gmail.com>
2022-11-24 19:57:46 +0530

    showing image user set

---
+++ block-BRaabd/my-app/src/components/App.js
@@ -0,0 +1,30 @@
+/** @format */
+
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <button> BasketBall</button>
+          <button>cricket</button>
+          <button>lapTop</button>
+          <button>phone</button>
+          <button>pubg</button>
+          <button>tiger</button>
+        </div>
+
+        <figure>
+          <img src="" alt="" />
+        </figure>
+      </div>
+    );
+  }
+}
+
+export default App;
