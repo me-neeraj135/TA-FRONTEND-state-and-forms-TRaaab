3a9b766792de975ccbe52fe24171c05a0b547b0c
neeraj <me.neeraj1455@gmail.com>
2022-11-24 20:39:41 +0530

    showing image using state

--- block-BRaabd/my-app/src/components/App.js
+++ block-BRaabd/my-app/src/components/App.js
@@ -5,24 +5,37 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      active: `basketball`,
+    };
   }
 
   render() {
+    let labels = [`basketball`, `cricket`, `laptop`, `phone`, `pubg`, `tiger`];
     return (
-      <div>
+      <>
         <div>
-          <button> BasketBall</button>
-          <button>cricket</button>
-          <button>lapTop</button>
-          <button>phone</button>
-          <button>pubg</button>
-          <button>tiger</button>
+          {labels.map(label => (
+            <button
+              key={label}
+              className={this.state.active === label ? `active` : ``}
+              onClick={() => {
+                this.setState({
+                  active: label,
+                });
+              }}
+            >
+              {label}
+            </button>
+          ))}
         </div>
-
-        <figure>
-          <img src="" alt="" />
-        </figure>
-      </div>
+        <div>
+          <img
+            src={`/images/${this.state.active}.jpg`}
+            alt={this.state.active}
+          />
+        </div>
+      </>
     );
   }
 }
