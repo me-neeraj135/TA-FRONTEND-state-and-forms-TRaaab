6f5e6c89457282b663e15e63b7fb2dfbf7cf093d
neeraj <me.neeraj1455@gmail.com>
2022-11-29 22:22:59 +0530

    form validation

---
+++ block-BRaabr/my-app/src/components/App.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import Form from "./Form";
+import Inputs from "./Inputs";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>validations</h1>
+      </header>
+
+      <main>
+        <Form />
+        <Inputs />
+      </main>
+    </div>
+  );
+}
+
+export default App;
